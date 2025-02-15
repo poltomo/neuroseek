b5542932a65b393cc77ae4cb57f1e58b8e79386c
sodaJar <sodaJar.gd@gmail.com>
2025-02-15 15:16:50 +0000

    ok

--- app.py
+++ app.py
@@ -40,8 +40,14 @@ def search():
     
     if query:
         results = solr.search(query, rows=10)
-    
+    print(list(results))
     return render_template_string(HTML_TEMPLATE, results=results)
 
 if __name__ == "__main__":
     app.run(debug=True,port=5000)
+    solr.add([
+        {
+            "id": "test_1",
+            "title": "test text 1",
+        }
+    ])
